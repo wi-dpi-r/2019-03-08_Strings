4c37d4704bc4666a181504b34f44507a42fab3af
Carl Frederick <carl.frederick@dpi.wi.gov>
2019-03-08 13:08:00 -0600

    presentation

---
+++ demoCodeFcns.R
@@ -0,0 +1,7 @@
+library(dpiR)
+
+code_DistNames(c("Madison", "West", "Middleton Cross Plains", "Oregon"), showBads = TRUE)
+
+code_SchNames("Elm Lawn Elementary", "3549", askBads = TRUE)
+
+code_SchNames("Oregon HS", "4144", askBads = TRUE)
